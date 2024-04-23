d5cd98036abde19cff522295d6466b1c68b04900
Alex Cajina <cajina17@gmail.com>
2024-04-22 21:10:43 -0600

    Archivos html,css,js

---
+++ Script.js
@@ -0,0 +1,20 @@
+
+// Selecciona los botones por su ID
+var aceptarBtn = document.getElementById("aceptarBtn");
+var rechazarBtn = document.getElementById("rechazarBtn");
+
+// Agrega un evento de clic a cada botón
+aceptarBtn.addEventListener("click", function() {
+    // Redirige a index.html
+    window.location.href = "corazon.html";
+});
+
+rechazarBtn.addEventListener("click", function() {
+    // Redirige a index.html
+    window.location.href = "Index2.html";
+});
+
+
+
+
+
